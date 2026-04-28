6adc9cc104e1921ced8cfd186b15ae243cbec0ff
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2026-04-27 23:19:04 -0300

    feat(backend): inicialização do esqueleto PredictDT via Spring Boot
    - Configuração do projeto com Java 21 e Maven;
    - Adição de dependências críticas: Spring Data JPA, PostgreSQL Driver e Flyway;
    - Integração com o ecossistema de dados para suporte ao TimescaleDB;
    - Preparação da estrutura de pacotes com o grupo com.unisal.predictdt.

---
+++ backend/predictdt/predictdt/src/main/java/com/unisal/predictdt/PredictdtApplication.java
@@ -0,0 +1,13 @@
+package com.unisal.predictdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictdtApplication.class, args);
+	}
+
+}
---
+++ backend/predictdt/predictdt/src/test/java/com/unisal/predictdt/PredictdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisal.predictdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
