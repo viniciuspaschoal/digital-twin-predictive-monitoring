5894ef93755257e7d0ccfd7141bda5385fa5a63b
Murillo Gonçalves <lillogon@outlook.com>
2026-04-29 21:08:36 -0300

    feat: adiciona entidades do domínio

---
+++ backend/predictdt/predictdt/src/main/java/com/unisal/predictdt/domain/equipamento/Equipamento.java
@@ -0,0 +1,39 @@
+package com.unisal.predictdt.domain.equipamento;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "equipamento")
+public class Equipamento {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", updatable = false)
+    private UUID id;
+
+    @Column(name = "descricao", nullable = false, length = 100)
+    private String descricao;
+
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo;
+
+    @Column(name = "dt_inclusao", nullable = false)
+    private LocalDateTime dtInclusao;
+
+    @Column(name = "dt_bloqueio")
+    private LocalDateTime dtBloqueio;
+
+    @Column(name = "dt_alteracao", nullable = false)
+    private LocalDateTime dtAlteracao;
+}
